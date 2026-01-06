a25fd21257f2d3eaf60da743dc43db69343be09f
Ian Page Miranda Maciel <179640858+ianpage08@users.noreply.github.com>
2026-01-06 09:45:59 -0300

    d

--- portal_do_aluno/lib/features/admin/presentation/pages/add_usuarios/professor_cadastro.dart
+++ portal_do_aluno/lib/features/admin/presentation/pages/add_usuarios/professor_cadastro.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:portal_do_aluno/core/utils/formatters.dart';
 import 'package:portal_do_aluno/features/admin/presentation/pages/add_usuarios/build_input.dart';
 
 class ProfessorCadastro extends StatelessWidget {
